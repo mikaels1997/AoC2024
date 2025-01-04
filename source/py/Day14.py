14ffa63d11a87a3658f4f4178444186290f8be33
Mikael Särkiniemi <mikaels1997@gmail.com>
2025-01-04 16:34:24 +0200

    Day 14 optimization

--- source/py/Day14.py
+++ source/py/Day14.py
@@ -1,23 +1,18 @@
 import re
-from collections import Counter
 from math import prod
 
-def solve(path, w, h):
-    rs = list(map(re.compile(r'-?\d+').findall, 
-                   open(path).read().splitlines()))
-    res = [0, 0, 0, 0]
-    for i in range(int(1e6)):
-        cs = []
-        for r in rs:
-            x, y, v1, v2 = map(int, r)
-            x = (x + v1 * i) % w
-            y = (y + v2 * i) % h
-            if i == 100 and x != w // 2 and y != h // 2:
-                res[2* (y > (h//2)) + (x > (w//2))] += 1
-            cs.append((x, y))
-        cnt = [Counter([c[i] for c in cs]) for i in [0,1]]
-        if sum([co >= 30 for co in cnt[0].values()]) >= 2:
-            if sum([co >= 30 for co in cnt[1].values()]) >= 2:
-                return [prod(res), i]
+rs = list(map(re.compile(r'-?\d+').findall, 
+                open("data/input14.txt").readlines()))
+res, w, h = [0, 0, 0, 0], 101, 103
 
-print(solve("data/input14.txt", 101, 103))
+for i in range(int(1e6)):
+    cnts = {c: 0 for c in range(w+h)}
+    for r in rs:
+        x, y, v1, v2 = map(int, r)
+        x, y = (x + v1 * i) % w, (y + v2 * i) % h
+        if i == 100 and x != w // 2 and y != h // 2:
+            res[2* (y > (h//2)) + (x > (w//2))] += 1
+        cnts[x] += 1; cnts[w+y] += 1
+    if sum([v >= 30 for v in cnts.values()]) >= 4:
+        print([prod(res), i])
+        break
