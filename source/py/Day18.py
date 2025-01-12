b90fb23e4668e93647f5f78f9defa0710d1386c5
Mikael Särkiniemi <mikaels1997@gmail.com>
2025-01-13 01:22:56 +0200

    Day18 refactor

--- source/py/Day18.py
+++ source/py/Day18.py
@@ -1,30 +1,24 @@
-bytes = [complex(*map(int, l.split(","))) 
+s, bytes = 71, [tuple(map(int, l.split(",")))
      for l in open("data/input18.txt").read().split()]
-grid = [complex(x, y) for y in range(71) for x in range(71)]
-s, g = grid[0], grid[-1]
 
-def bfs(G, root, goal):
-    Q, vis, p = [root], [root], {}
+def bfs(lim):
+    Q, p, vis = [(0, 0)], {}, set([(0, 0)] + bytes[0:lim])
     while len(Q) > 0:
         v = Q.pop(0)
-        if v == goal:
-            node, d = p[goal], 1
-            while node != root:
-                node = p[node]
-                d += 1 
+        if v == (s, s):
+            n, d = p[(s, s)], 1
+            while n != (0, 0): n, d = p[n], d+1 
             return d
-        for n in [1, 1j, -1, -1j]:
-            ne = v + n
-            if ne not in vis + G and ne in grid:
+        for n in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+            ne = (v[0] + n[0], v[1] + n[1])
+            if ne not in vis and all(0 <= c <= s for c in ne):
                 p[ne] = v
-                vis.append(ne)
-                Q.append(ne)
+                vis.add(ne); Q.append(ne)
 
-a, t, expl = len(bytes) // 2, len(bytes) // 4, []
-while a not in expl:
-    expl.append(a)
-    a += t if bool(bfs(bytes[0:a], s, g)) else -t
+a, t, vis = len(bytes) // 2, len(bytes) // 4, []
+while a not in vis:
+    vis.append(a)
+    a += t if bool(bfs(a)) else -t
     if t != 1: t //= 2
 
-print(bfs(bytes[0: 1024], s, g))
-print(f'{str(int(bytes[a].real))},{str(int(bytes[a].imag))}')
+print(bfs(1024), ','.join(map(str, bytes[a])))
